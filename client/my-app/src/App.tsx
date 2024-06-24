891680b30aa84ebba8fb2fe48504dc48aab84df6
Tiago Vieira <tiagoj2006@hotmail.com>
2024-06-24 19:42:04 +0100

    old2

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -3,7 +3,9 @@ import AppLayout from '@components/layouts/AppLayout'
 import About from '@components/templates/About'
 import Contact from '@components/templates/Contact'
 import Home from '@components/templates/Home'
+import Login from '@components/templates/Login'
 import Products from '@components/templates/Products'
+import SignUp from '@components/templates/SignUp'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App: FC = () => {
@@ -16,6 +18,8 @@ const App: FC = () => {
           <Route path="/products" element={<Products />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/teste" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign_up" element={<SignUp />} />
         </Routes>
       </AppLayout>
     </Router>
