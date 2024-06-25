7849aa92981293ef7e2e0717e20e03e236f8258e
duartecouto <duarte.schwarz.roncon.couto@gmail.com>
2024-06-25 17:29:23 +0100

    Tudo ok!

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -6,26 +6,10 @@ import Home from '@components/templates/Home'
 import Login from '@components/templates/Login'
 import Products from '@components/templates/Products'
 import SignUp from '@components/templates/SignUp'
-import Login from '@components/templates/Login'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
-
+import { Route, Routes } from 'react-router-dom'
 
 const App: FC = () => {
   return (
-    <Router>
-      <AppLayout>
-        <Routes>
-          <Route path="/" element={<Home title="Home" subtitle="Subtitle" />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/teste" element={<Contact />} />
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/login" element={<Login/>} />
-        </Routes>
-      </AppLayout>
-    </Router>
     <AppLayout>
       <Routes>
         <Route path="/" element={<Home />} />
--- server/models/users.js
+++ server/models/users.js
@@ -3,21 +3,18 @@ module.exports = (sequelize, DataTypes) => {
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true, 
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true, 
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
 });
-
- // Users.associate = (models) => {
-   // Users.has
-
-  //}
-
-
-
-
-
   return Users;
 };
+
